15245a5de78dd7959666e5921ae89e9285974598
azkarmoulana <azkar.moulana@gmail.com>
2018-11-20 21:56:32 +0530

    libs, grid api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'resizable-table';
+export class AppComponent implements OnInit{
+  
+  private colmDfs;
+  private gridApi;
+  private gridColumnApi;
+  private sortingOrd;
+
+  constructor(private htttp: HttpClient){
+
+  }
+
+  ngOnInit(){
+
+  }
 }
