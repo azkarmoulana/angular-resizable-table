58608f4cc183ed5def2a65b2686be64fad326d06
azkarmoulana <azkar.moulana@gmail.com>
2018-11-25 14:35:55 +0530

    ag-grid data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,23 @@ export class AppComponent implements OnInit{
   private gridColumnApi;
   private sortingOrd;
 
-  constructor(private htttp: HttpClient){
+  constructor(){
 
   }
 
   ngOnInit(){
 
   }
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make' },
+    {headerName: 'Model', field: 'model' },
+    {headerName: 'Price', field: 'price'}
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
 }
